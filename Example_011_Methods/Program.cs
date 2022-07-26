d36fd940862e38acc021face6c0c1b8fad8cd1b2
vasilakiy <slk55slk@gmail.com>
2022-07-26 16:59:13 +0300

    Добавлены циклы и методы

---
+++ Example_011_Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿// 1 вид методов, который ничего не возвращает и не принимает никаких аргументов, может просто например что-то выводить на экран
+void Method1()
+{
+  Console.WriteLine("Пример сообщения");
+}
+// вызов метода
+Method1();
+
+
+
+// 2 вид методов, который ничего не возвращает, но может принимать аргументы
+void Method2(string msg, int count)
+{
+  int i = 0;
+  while (i < count)
+  {
+    Console.WriteLine(msg);
+    i++;
+  }
+
+}
+Method2(msg: "Текст вызванн методом msg четыре раза", count: 4);
+
+
+// 3 вид методов, которые что-то возвращают, но ничего не принимают
+int Method3()
+{
+  return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+// 4 вид методов , которые что-то принимают и что-то возвращают
+string Method4(int count, string text)
+{
+  int i = 0;
+  string result = String.Empty; // string result = "";
+  while (i < count)
+  {
+    result = result + text; // for(int i = 0; i < count; i++)
+    i++;
+  }
+  return result;
+}
+
+string res = Method4(10, "Пример"); // string переменная res = Метод Method4(count = 10, string text = "Пример")
+Console.WriteLine(res);
+
+// Цикл for
+// Таблица умножения
+for (int i = 2; i <= 10; i++)
+{
+  for (int j = 2; j <= 10; j++)
+  {
+    Console.WriteLine($"{i} * {j} = {i * j}");
+  }
+  Console.WriteLine();
+}
---
+++ Example_013_Massiv/Program.cs
@@ -0,0 +1,35 @@
+﻿// Работа с массивами. Перебор массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array) // Метод , который выводит в консоль заданный нами массив
+{
+  int count = array.Length;
+
+  for (int i = 0; i < count; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    int minPosition = i; // вводим переменную minPosition и присваиваем ей значение i
+
+    for (int j = i + 1; j < array.Length; j++)
+    {
+      if (array[j] < array[minPosition]) minPosition = j;
+    }
+
+    int temp = array[i]; // вводим переменную temp и присваиваем ей значение в текущем индексе массива
+    array[i] = array[minPosition]; // текущему значению в текущем индексе массива присваиваем значение лежащее в индексе minPosition
+    array[minPosition] = temp; // текущему значению в индексе minPosition присваиваем значение лежащее в temp
+  }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
